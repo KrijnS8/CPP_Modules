fa0128f5978aabfbc5145f951c666815cba082c7
Krijn Schelvis <kschelvi@f0r6s10.codam.nl>
2024-03-01 12:27:18 +0100

    finished ex00

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -6,17 +6,32 @@
 /*   By: kschelvi <kschelvi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/29 15:46:52 by kschelvi      #+#    #+#                 */
-/*   Updated: 2024/02/29 16:02:32 by kschelvi      ########   odam.nl         */
+/*   Updated: 2024/03/01 12:26:17 by kschelvi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.h"
 #include <iostream>
 
-Fixed::Fixed( void ): _value(0) { std::cout << "Default constructor called\n"; };
-Fixed::Fixed( const F& other ): _value(other._value) { std::cout << "Copy constructor called\n"; }
-Fixed::~Fixed( void ) { std::cout << "Destructor called\n"; }
+// Default constructor
+Fixed::Fixed( void ): _value(0)
+{
+	std::cout << "Default constructor called\n";
+}
 
+// Copy constructor
+Fixed::Fixed( const F& other ): _value(other._value)
+{
+	std::cout << "Copy constructor called\n";
+}
+
+// Default destructor
+Fixed::~Fixed( void )
+{
+	std::cout << "Destructor called\n";
+}
+
+// Copy assignment operator overload
 Fixed& Fixed::operator=(const F& other)
 {
 	std::cout << "Copy assignment operator called\n";
