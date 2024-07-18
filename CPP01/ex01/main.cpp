6e1581d5dcadc2d4a601a649769ea16b20ab168c
Krijn Schelvis <kschelvi@f1r5s3.codam.nl>
2024-07-18 12:52:33 +0200

    fix

--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kschelvi <kschelvi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/12 17:32:40 by kschelvi      #+#    #+#                 */
-/*   Updated: 2024/02/12 17:37:53 by kschelvi      ########   odam.nl         */
+/*   Updated: 2024/07/18 12:52:14 by kschelvi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 int main(void) {
 	int		nbZombies = 10;
 	Zombie* zombies = zombieHorde(nbZombies, "Zombie");
+	if (!zombies)
+		return (1);
 	for (int i = 0; i < nbZombies; i ++)
 		zombies[i].annouce();
 	delete[] zombies;
--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: kschelvi <kschelvi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/12 17:09:45 by kschelvi      #+#    #+#                 */
-/*   Updated: 2024/02/12 17:32:39 by kschelvi      ########   odam.nl         */
+/*   Updated: 2024/07/18 12:51:55 by kschelvi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 Zombie* zombieHorde( int N, std::string name ) {
 	Zombie* ptr = new Zombie[N];
 
+	if (!ptr)
+		return (NULL);
 	for (int i = 0; i < N; i++)
 		ptr[i].setName(name);
 	return (ptr);
